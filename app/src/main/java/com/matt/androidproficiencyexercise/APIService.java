a54e4c2e6753df7248558fe9eba641ebc72fb0eb
MattPetrenas <matthew.petrenas@gmail.com>
2018-02-23 12:01:18 +1100

    Feed now loaded by API request in onCreate()
    Using an IntentService with a BroadcastReceiver to retrieve data.

--- app/src/main/java/com/matt/androidproficiencyexercise/APIService.java
+++ app/src/main/java/com/matt/androidproficiencyexercise/APIService.java
@@ -1,96 +1,106 @@
 package com.matt.androidproficiencyexercise;
 
-import org.json.JSONException;
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONObject;
 
+
 /**
- * Created by Matthew Petrenas on 22/02/2018.
- * Placeholder class for the API Integration.
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
  */
+public class APIService extends IntentService {
+    private static final String ACTION_GET_FEED = "com.matt.androidproficiencyexercise.action.getFeed";
 
-public class APIService {
-    public static String payload = "{\n" +
-            "\"title\":\"About Canada\",\n" +
-            "\"rows\":[\n" +
-            "\t{\n" +
-            "\t\"title\":\"Beavers\",\n" +
-            "\t\"description\":\"Beavers are second only to humans in their ability to manipulate and change their environment. They can measure up to 1.3 metres long. A group of beavers is called a colony\",\n" +
-            "\t\"imageHref\":\"http://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/220px-American_Beaver.jpg\"\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Flag\",\n" +
-            "\t\"description\":null,\n" +
-            "\t\"imageHref\":\"http://images.findicons.com/files/icons/662/world_flag/128/flag_of_canada.png\"\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Transportation\",\n" +
-            "\t\"description\":\"It is a well known fact that polar bears are the main mode of transportation in Canada. They consume far less gas and have the added benefit of being difficult to steal.\",\n" +
-            "\t\"imageHref\":\"http://1.bp.blogspot.com/_VZVOmYVm68Q/SMkzZzkGXKI/AAAAAAAAADQ/U89miaCkcyo/s400/the_golden_compass_still.jpg\"\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Hockey Night in Canada\",\n" +
-            "\t\"description\":\"These Saturday night CBC broadcasts originally aired on radio in 1931. In 1952 they debuted on television and continue to unite (and divide) the nation each week.\",\n" +
-            "\t\"imageHref\":\"http://fyimusic.ca/wp-content/uploads/2008/06/hockey-night-in-canada.thumbnail.jpg\"\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Eh\",\n" +
-            "\t\"description\":\"A chiefly Canadian interrogative utterance, usually expressing surprise or doubt or seeking confirmation.\",\n" +
-            "\t\"imageHref\":null\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Housing\",\n" +
-            "\t\"description\":\"Warmer than you might think.\",\n" +
-            "\t\"imageHref\":\"http://icons.iconarchive.com/icons/iconshock/alaska/256/Igloo-icon.png\"\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Public Shame\",\n" +
-            "\t\"description\":\" Sadly it's true.\",\n" +
-            "\t\"imageHref\":\"http://static.guim.co.uk/sys-images/Music/Pix/site_furniture/2007/04/19/avril_lavigne.jpg\"\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":null,\n" +
-            "\t\"description\":null,\n" +
-            "\t\"imageHref\":null\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Space Program\",\n" +
-            "\t\"description\":\"Canada hopes to soon launch a man to the moon.\",\n" +
-            "\t\"imageHref\":\"http://files.turbosquid.com/Preview/Content_2009_07_14__10_25_15/trebucheta.jpgdf3f3bf4-935d-40ff-84b2-6ce718a327a9Larger.jpg\"\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Meese\",\n" +
-            "\t\"description\":\"A moose is a common sight in Canada. Tall and majestic, they represent many of the values which Canadians imagine that they possess. They grow up to 2.7 metres long and can weigh over 700 kg. They swim at 10 km/h. Moose antlers weigh roughly 20 kg. The plural of moose is actually 'meese', despite what most dictionaries, encyclopedias, and experts will tell you.\",\n" +
-            "\t\"imageHref\":\"http://caroldeckerwildlifeartstudio.net/wp-content/uploads/2011/04/IMG_2418%20majestic%20moose%201%20copy%20(Small)-96x96.jpg\"\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Geography\",\n" +
-            "\t\"description\":\"It's really big.\",\n" +
-            "\t\"imageHref\":null\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Kittens...\",\n" +
-            "\t\"description\":\"Éare illegal. Cats are fine.\",\n" +
-            "\t\"imageHref\":\"http://www.donegalhimalayans.com/images/That%20fish%20was%20this%20big.jpg\"\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Mounties\",\n" +
-            "\t\"description\":\"They are the law. They are also Canada's foreign espionage service. Subtle.\",\n" +
-            "\t\"imageHref\":\"http://3.bp.blogspot.com/__mokxbTmuJM/RnWuJ6cE9cI/AAAAAAAAATw/6z3m3w9JDiU/s400/019843_31.jpg\"\n" +
-            "\t},\n" +
-            "\t{\n" +
-            "\t\"title\":\"Language\",\n" +
-            "\t\"description\":\"Nous parlons tous les langues importants.\",\n" +
-            "\t\"imageHref\":null\n" +
-            "\t}\n" +
-            "]\n" +
-            "}";
+    private static final String EXTRA_RESPONSE_RECIEVER = "com.matt.androidproficiencyexercise.extra.ResponseReciever";
 
-    public static JSONObject getJson(){
-        try {
-            return new JSONObject(payload);
-        } catch (JSONException e) {
-            e.printStackTrace();
+    //Request Status Broadcast Extras
+    public static final String STATUS = "status";
+    public static final String RESPONSE = "response";
+
+    //Request Statuses
+    public static final String ERROR = "error";
+    public static final String IN_PROGRESS = "in progress";
+    public static final String FINISHED = "finished";
+
+    public static final String FACTS_URI = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json";
+
+    private RequestQueue requestQueue;
+
+    public APIService() {
+        super("APIService");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    public static void startActionGetFeed(Context context, String responseReciever) {
+        Intent intent = new Intent(context, APIService.class);
+        intent.setAction(ACTION_GET_FEED);
+        intent.putExtra(EXTRA_RESPONSE_RECIEVER, responseReciever);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (requestQueue == null){requestQueue = Volley.newRequestQueue(this);}
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_GET_FEED.equals(action)) {
+                final String reciever = intent.getStringExtra(EXTRA_RESPONSE_RECIEVER);
+                handleActionGetFeed(reciever);
+            }
         }
-        return null;
+    }
+
+    private void handleActionGetFeed(final String reciever) {
+            final String uri = FACTS_URI;
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                    (Request.Method.POST, uri, null, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d("Response", response.toString());
+                                Intent broadcastIntent = new Intent();
+                                broadcastIntent.setAction(reciever);
+                                broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+                                broadcastIntent.putExtra(STATUS, FINISHED);
+                                broadcastIntent.putExtra(RESPONSE, response.toString());
+                                sendBroadcast(broadcastIntent);
+                            }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            if(error.networkResponse!=null) {
+                                Log.d("Error Body", error.networkResponse.toString());
+                            }
+                            else{
+                                Log.d("Error", error.getMessage());
+                            }
+                            broadcastStatusOnly(reciever, ERROR);
+                        }
+                    });
+            requestQueue.add(jsObjRequest);
+        }
+
+    private void broadcastStatusOnly(String reciever, String value) {
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction(reciever);
+        broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+        broadcastIntent.putExtra(STATUS, value);
+        sendBroadcast(broadcastIntent);
     }
 }
--- app/src/main/java/com/matt/androidproficiencyexercise/MainActivity.java
+++ app/src/main/java/com/matt/androidproficiencyexercise/MainActivity.java
@@ -1,8 +1,11 @@
 package com.matt.androidproficiencyexercise;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,9 +24,13 @@ import java.util.ArrayList;
  */
 
 public class MainActivity extends AppCompatActivity {
-    private TextView feedTitleView;
+    private TextView feedTitleView, requestStatusView;
     private ListView feedRowsListView;
     private FeedRowListAdapter feedRowListAdapter;
+
+    private SyncReceiver syncReceiver;
+    private boolean recieverRegistered = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,30 +38,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Initialise the UI Elements before making API calls.
         feedTitleView = (TextView) findViewById(R.id.feed_title);
+        requestStatusView = (TextView) findViewById(R.id.request_status);
         feedRowsListView = (ListView) findViewById(R.id.feed_list_view);
         downloadFeed();
+        if(!recieverRegistered) {
+            IntentFilter filter = new IntentFilter(SyncReceiver.receiver);
+            filter.addCategory(Intent.CATEGORY_DEFAULT);
+            SyncReceiver syncReceiver = new SyncReceiver();
+            registerReceiver(syncReceiver, filter);
+            recieverRegistered=true;
+        }
     }
 
     private void downloadFeed() {
         //This function will make an API call via the API Service via a broadcast.
-
+        APIService.startActionGetFeed(this, SyncReceiver.receiver);
         //Temp Code for placeholder:
         //This Code Parses the JSON resposne.
-        JSONObject json = APIService.getJson();
-        Log.d("TEST", json.toString());
-        try {
-            //Convert from jSONArray  to ArrayList<Model>
-            ArrayList<FeedRow> listdata = new ArrayList<FeedRow>();
-            JSONArray jArray = json.getJSONArray("rows");
-            if (jArray != null) {
-                for (int i = 0; i < jArray.length(); i++) {
-                    listdata.add(new FeedRow(jArray.getJSONObject(i)));
-                }
-            }
-            loadFeed(json.getString("title"), listdata);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 
     private void loadFeed(String title, ArrayList<FeedRow> rows) {
@@ -62,6 +62,74 @@ public class MainActivity extends AppCompatActivity {
         feedTitleView.setText(title);
         feedRowListAdapter = new FeedRowListAdapter(rows, this);
         feedRowsListView.setAdapter(feedRowListAdapter);
+
+    }
+
+    public class SyncReceiver extends BroadcastReceiver {
+        public static final String receiver =  "MainActivity.ReponseReciever";
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String status = intent.getStringExtra(APIService.STATUS);
+            switch(status){
+                case APIService.ERROR:
+                    processError();
+                    break;
+                case APIService.IN_PROGRESS:
+                    showProgress();
+                    break;
+                case APIService.FINISHED:
+                    processResult(context, intent);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void processError(){
+            requestStatusView.setText("ERROR!");
+        }
+        private void showProgress(){
+            requestStatusView.setText("Syncing...");
+        }
+
+        private void processResult(Context context, Intent intent){
+            requestStatusView.setText("Done.");
+            String rawPayload = intent.getStringExtra(APIService.RESPONSE);
+            try {
+                JSONObject jsonPayload = new JSONObject(rawPayload);
+                //Convert from jSONArray  to ArrayList<Model>
+                ArrayList<FeedRow> listdata = new ArrayList<FeedRow>();
+                JSONArray jArray = jsonPayload.getJSONArray("rows");
+                if (jArray != null) {
+                    for (int i = 0; i < jArray.length(); i++) {
+                        listdata.add(new FeedRow(jArray.getJSONObject(i)));
+                    }
+                }
+                loadFeed(jsonPayload.getString("title"), listdata);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause()
+    {
+        recieverRegistered = false;
+        unregisterReceiver(syncReceiver);
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if(!recieverRegistered) {
+            IntentFilter filter = new IntentFilter(SyncReceiver.receiver);
+            filter.addCategory(Intent.CATEGORY_DEFAULT);
+            syncReceiver = new SyncReceiver();
+            registerReceiver(syncReceiver, filter);
+            recieverRegistered=true;
+        }
 
     }
 }
